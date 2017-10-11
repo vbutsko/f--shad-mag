fe31ee05753b981f62445ee54942635f96b8ecad
Uladzislau Butsko <butko.wladislaw@gmail.com>
2017-10-11 21:15:31 +0300

    02 lab init

---
+++ lab2/hw2_sample.fs
@@ -0,0 +1,14 @@
+﻿module hw2
+
+type Name = {first: string; middle: string; last: string}
+
+
+
+type suit = Clubs | Diamonds | Hearts | Spades
+type rank = Jack | Queen | King | Ace | Num of int
+type card = suit * rank
+
+type color = Red | Black
+type move = Discard of card | Draw
+
+exception IllegalMove
---
+++ lab2/test_sampe.fs
@@ -0,0 +1,48 @@
+﻿module test
+open hw2;;
+
+(* 
+Some trivial tests are allredy done for you. You should add more tests here.
+Although tests won't be graded, make sure that you solution is tested well.
+*)
+
+exception NoSuchCard
+
+let tests () =
+(* Task 1*)
+    printfn "all_except_option:"
+    printfn "Test 1: %b" (all_except_option("", []) = None)
+    printfn "get_substitutions1:"
+    printfn "Test 1: %b" (get_substitutions1([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie";"Fred";"F"]], "Fred") = ["Fredrick"; "Freddie"; "F"])
+    printfn "get_substitutions2:"
+    printfn "Test 1: %b" (get_substitutions2([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie";"Fred";"F"]], "Fred") = ["Fredrick"; "Freddie"; "F"])
+    printfn "similar_names:"
+    printfn "Test 1: %b" (similar_names([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie"; "Fred"; "F"]], {first="Fred"; middle="W"; last="Smith"}) = 
+                            [{first="Fred"; last="Smith"; middle="W"}; {first="Fredrick"; last="Smith"; middle="W"};
+                            {first="Freddie"; last="Smith"; middle="W"};
+                            {first="F"; last="Smith"; middle="W"}])
+(* Task 2*)
+    printfn "card_color:"
+    printfn "Test 1: %b" (card_color(Clubs, Jack) = Black)
+    printfn "card_value:"
+    printfn "Test 1: %b" (card_value(Clubs, Num 7) = 7)
+    printfn "remove_card:"
+    printfn "Test 1: %b" (remove_card([(Clubs, Jack)], (Clubs, Jack), NoSuchCard) = [])
+    printfn "Test 2: %b" (try ignore (remove_card([], (Clubs, Jack), NoSuchCard)); false
+                          with NoSuchCard -> true | _ -> false)
+    printfn "all_same_color:"
+    printfn "Test1: %b" (all_same_color([]) = true)
+    printfn "sum_cards:"
+    printfn "Test1: %b" (sum_cards([(Clubs, Num 7)]) = 7)
+    printfn "score:"
+    printfn "Test1: %b" (score([(Clubs, Num 7)], 7) = 0)
+    printfn "Test2: %b" (score([(Hearts, Num 2); (Clubs, Num 4)], 10) = 4)
+    printfn "officiate:"
+    printfn "Test1: %b" (officiate ([(Hearts, Num 2); (Clubs, Num 4)], [Draw], 15) = 6)
+    printfn "Test2: %b" (officiate([(Clubs,Ace); (Spades,Ace); (Clubs,Ace); (Spades,Ace)],
+                        [Draw; Draw; Draw; Draw; Draw], 42) =
+                        3)
+    printfn "Test3: %b" (try 
+                            ignore (officiate([(Clubs, Jack); (Spades, Num(8))], [Draw; Discard(Hearts, Jack)],42));
+                            false
+                         with IllegalMove -> true | _ -> false)
