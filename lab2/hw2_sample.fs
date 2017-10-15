ab4a42bf3d8ba70bdc73053a868d3ad628e9f533
Uladzislau Butsko <butko.wladislaw@gmail.com>
2017-10-15 14:06:03 +0300

    lab 02 task 1

--- lab2/hw2_sample.fs
+++ lab2/hw2_sample.fs
@@ -12,3 +12,49 @@ type color = Red | Black
 type move = Discard of card | Draw
 
 exception IllegalMove
+
+let rec all_except_option(name: string, name_list: string list)=
+    match name_list with
+        | [] -> None
+        | first_name::name_list' -> 
+            if first_name = name
+            then Some(name_list') 
+            else match all_except_option(name, name_list') with         
+                | Some names -> Some(first_name::names)
+                | None -> None
+
+let rec get_substitutions1(substitutions: string list list, str: string) : string list =
+    match substitutions with
+        | [] -> []
+        | sub::substitutions' -> 
+            match all_except_option(str, sub) with 
+                | None -> get_substitutions1(substitutions', str)
+                | Some(result) -> result@get_substitutions1(substitutions', str)
+            
+let get_substitutions2(substitutions: string list list, str: string) : string list =
+    let rec aux(substitutions, result_list) = 
+        match substitutions with
+            | [] -> result_list
+            | sub::substitutions' -> 
+                match all_except_option(str, sub) with
+                    | None -> aux(substitutions', result_list)
+                    | Some(result) -> aux(substitutions', result_list@result)
+    in aux(substitutions, [])
+    
+let rec similar_names(substitutions, full_name) =
+    match full_name with
+        |{first = f; middle = m; last = l} ->
+            let rec aux(names : string list, result_list : Name list) =
+                match names with
+                    | [] -> result_list
+                    | name::names' -> aux(names', result_list@[{first = name; middle = m; last = l}])
+            in aux(get_substitutions2(substitutions, f), [{first = f; middle = m; last = l}])
+            
+                
+(*
+let card_color( card_1: card ) =
+    match card_1 with 
+        |suit rank
+        |
+    *)
+    
--- lab2/test_sampe.fs
+++ lab2/test_sampe.fs
@@ -12,17 +12,33 @@ let tests () =
 (* Task 1*)
     printfn "all_except_option:"
     printfn "Test 1: %b" (all_except_option("", []) = None)
+    printfn "Test 2: %b" (all_except_option("name", ["Bob"; "Jon"; "Loky"]) = None)
+    printfn "Test 3: %b" (all_except_option("Bob", ["Bob"; "Jon"; "Loky"]) = Some(["Jon";"Loky"]))
+    printfn "Test 4: %b" (all_except_option("Jon", ["Bob"; "Jon"; "Loky"]) = Some(["Bob";"Loky"]))
+    printfn "Test 5: %b" (all_except_option("Loky", ["Bob"; "Jon"; "Loky"]) = Some(["Bob";"Jon"]))
     printfn "get_substitutions1:"
     printfn "Test 1: %b" (get_substitutions1([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie";"Fred";"F"]], "Fred") = ["Fredrick"; "Freddie"; "F"])
+    printfn "Test 2: %b" (get_substitutions1([["Fred"; "Fredrick"]; ["Elizabeth";"Betty";"Fred"]; ["Freddie";"Fred";"F"]], "Fred") = ["Fredrick";"Elizabeth";"Betty";"Freddie"; "F"])
+    printfn "Test 3: %b" (get_substitutions1([["Fred"; "Fredrick"]; ["Elizabeth";"Betty";]; ["Freddie";"Fred";"F"]], "Betty") = ["Elizabeth"])
+    printfn "Test 4: %b" (get_substitutions1([["Fred"; "Fredrick"]; ["Elizabeth";"Betty";]; ["Freddie";"Fred";"F"]], "Jon") = [])
+    printfn "Test 5: %b" (get_substitutions1([["Fredrick"]; ["Elizabeth";"Betty";]; ["Freddie";"Fred";"F"]], "Fredrick") = [])
+    printfn "Test 6: %b" (get_substitutions1([[]; []], "Fredrick") = [])
+    printfn "Test 7: %b" (get_substitutions1([[]; []], "") = [])
     printfn "get_substitutions2:"
     printfn "Test 1: %b" (get_substitutions2([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie";"Fred";"F"]], "Fred") = ["Fredrick"; "Freddie"; "F"])
+    printfn "Test 2: %b" (get_substitutions2([["Fred"; "Fredrick"]; ["Elizabeth";"Betty";"Fred"]; ["Freddie";"Fred";"F"]], "Fred") = ["Fredrick";"Elizabeth";"Betty";"Freddie"; "F"])
+    printfn "Test 3: %b" (get_substitutions2([["Fred"; "Fredrick"]; ["Elizabeth";"Betty";]; ["Freddie";"Fred";"F"]], "Betty") = ["Elizabeth"])
+    printfn "Test 4: %b" (get_substitutions2([["Fred"; "Fredrick"]; ["Elizabeth";"Betty";]; ["Freddie";"Fred";"F"]], "Jon") = [])
+    printfn "Test 5: %b" (get_substitutions2([["Fredrick"]; ["Elizabeth";"Betty";]; ["Freddie";"Fred";"F"]], "Fredrick") = [])
+    printfn "Test 6: %b" (get_substitutions2([[]; []], "Fredrick") = [])
+    printfn "Test 7: %b" (get_substitutions2([[]; []], "") = [])
     printfn "similar_names:"
     printfn "Test 1: %b" (similar_names([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie"; "Fred"; "F"]], {first="Fred"; middle="W"; last="Smith"}) = 
                             [{first="Fred"; last="Smith"; middle="W"}; {first="Fredrick"; last="Smith"; middle="W"};
                             {first="Freddie"; last="Smith"; middle="W"};
                             {first="F"; last="Smith"; middle="W"}])
 (* Task 2*)
-    printfn "card_color:"
+    (*printfn "card_color:"
     printfn "Test 1: %b" (card_color(Clubs, Jack) = Black)
     printfn "card_value:"
     printfn "Test 1: %b" (card_value(Clubs, Num 7) = 7)
@@ -45,4 +61,6 @@ let tests () =
     printfn "Test3: %b" (try 
                             ignore (officiate([(Clubs, Jack); (Spades, Num(8))], [Draw; Discard(Hearts, Jack)],42));
                             false
-                         with IllegalMove -> true | _ -> false)
+                         with IllegalMove -> true | _ -> false) *)
+                         
+tests()
