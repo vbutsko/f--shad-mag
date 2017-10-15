4b0dc490ec4078e005eb962c396d6294a60e4e61
Uladzislau Butsko <butko.wladislaw@gmail.com>
2017-10-15 20:59:51 +0300

    lab 02 task 2

--- lab2/hw2_sample.fs
+++ lab2/hw2_sample.fs
@@ -51,10 +51,73 @@ let rec similar_names(substitutions, full_name) =
             in aux(get_substitutions2(substitutions, f), [{first = f; middle = m; last = l}])
             
                 
-(*
-let card_color( card_1: card ) =
-    match card_1 with 
-        |suit rank
-        |
-    *)
+
+let card_color( (suit, rank) : card) =
+    match suit with 
+        | Clubs | Spades -> Black
+        | Diamonds | Hearts -> Red
+        
+let card_value((suit, rank) : card) =
+    match rank with
+        |Jack | Queen | King -> 10
+        |Ace -> 11
+        |Num(x) -> x 
+        
+let rec remove_card(cs: card list, (suit, rank) : card, e) =
+    match cs with
+        | [] -> raise e
+        | (s,r)::cs' -> 
+            if s = suit && r = rank
+            then
+                cs'
+            else
+                (s,r)::remove_card(cs', (suit, rank), e)
+                
+let rec all_same_color(cs) = 
+    match cs with 
+    | [] -> true
+    | c::cs ->
+        let colr = card_color(c)
+        let rec same_color(cs_) =
+            match cs_ with
+                | [] -> true
+                | c::cs_ -> 
+                    if card_color(c) = colr
+                    then same_color(cs_)
+                    else false
+        in same_color(cs)
     
+let sum_cards(cs) =
+    let rec aux(cs, sum) =
+        match cs with
+            | [] -> sum
+            | c::cs -> aux(cs, sum + card_value(c))
+    in aux(cs, 0)
+    
+let score(held_cards, goal) =
+    let sum = sum_cards(held_cards)
+    let total_score = 
+        if sum > goal 
+        then 3 * (sum - goal)
+        else goal - sum
+    in  if all_same_color(held_cards)
+        then total_score / 2
+        else total_score
+        
+let officiate(cs, ml, goal) =
+    let rec make_move(held_cards, moves) =
+        match moves with
+            | [] -> score(held_cards, goal)
+            | mv::moves ->
+                match mv with
+                    | Discard c ->
+                        make_move(remove_card(held_cards, c, IllegalMove), moves)
+                    | Draw ->
+                        match cs with 
+                            | [] -> score(held_cards, goal)
+                            | c::cs -> 
+                                if sum_cards(c::held_cards) > goal
+                                then score(c::held_cards, goal)
+                                else make_move(c::held_cards, moves)
+    in make_move([], ml)
+        
--- lab2/test_sampe.fs
+++ lab2/test_sampe.fs
@@ -37,22 +37,54 @@ let tests () =
                             [{first="Fred"; last="Smith"; middle="W"}; {first="Fredrick"; last="Smith"; middle="W"};
                             {first="Freddie"; last="Smith"; middle="W"};
                             {first="F"; last="Smith"; middle="W"}])
+    printfn "Test 2: %b" (similar_names([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie"; "Fred"; "F"]], {first="Elizabeth"; middle="W"; last="Smith"}) = 
+                                [{first="Elizabeth"; last="Smith"; middle="W"}; {first="Betty"; last="Smith"; middle="W"}])
+    printfn "Test 3: %b" (similar_names([["Fred"; "Fredrick"]; ["Betty";"Elizabeth"]; ["Freddie"; "Fred"; "F"]], {first="Elizabeth"; middle="W"; last="Smith"}) = 
+                                    [{first="Elizabeth"; last="Smith"; middle="W"}; {first="Betty"; last="Smith"; middle="W"}])
+    printfn "Test 4: %b" (similar_names([["Fred"; "Fredrick"]; ["Elizabeth";"Betty"]; ["Freddie"; "Fred"; "F"]], {first="Jon"; middle="W"; last="Smith"}) = 
+                                 [{first="Jon"; last="Smith"; middle="W"}])                               
 (* Task 2*)
-    (*printfn "card_color:"
-    printfn "Test 1: %b" (card_color(Clubs, Jack) = Black)
+    printfn "card_color:"
+    printfn "Test1: %b" (card_color(Clubs, Jack) = Black)
+    printfn "Test2: %b" (card_color(Diamonds, Jack) = Red)
+    printfn "Test3: %b" (card_color(Hearts, Jack) = Red)
+    printfn "Test4: %b" (card_color(Spades, Jack) = Black)
     printfn "card_value:"
-    printfn "Test 1: %b" (card_value(Clubs, Num 7) = 7)
+    printfn "Test1: %b" (card_value(Clubs, Num 6) = 6)
+    printfn "Test2: %b" (card_value(Clubs, Num 7) = 7)
+    printfn "Test3: %b" (card_value(Clubs, Num 8) = 8)
+    printfn "Test4: %b" (card_value(Clubs, Num 9) = 9)
+    printfn "Test5: %b" (card_value(Clubs, Num 10) = 10)
+    printfn "Test6: %b" (card_value(Clubs, Jack) = 10)
+    printfn "Test7: %b" (card_value(Clubs, Queen) = 10)
+    printfn "Test8: %b" (card_value(Clubs, King) = 10)
+    printfn "Test9: %b" (card_value(Clubs, Ace) = 11)
     printfn "remove_card:"
-    printfn "Test 1: %b" (remove_card([(Clubs, Jack)], (Clubs, Jack), NoSuchCard) = [])
-    printfn "Test 2: %b" (try ignore (remove_card([], (Clubs, Jack), NoSuchCard)); false
+    printfn "Test1: %b" (remove_card([(Clubs, Jack)], (Clubs, Jack), NoSuchCard) = [])
+    printfn "Test2: %b" (remove_card([(Clubs, Ace); (Clubs, Jack); (Hearts, Jack); (Clubs, Jack)], (Clubs, Jack), NoSuchCard) = [(Clubs, Ace);(Hearts, Jack); (Clubs, Jack)])
+    printfn "Test3: %b" (try ignore (remove_card([], (Clubs, Jack), NoSuchCard)); false
+                          with NoSuchCard -> true | _ -> false)
+    printfn "Test4: %b" (try ignore (remove_card([(Clubs, Ace); (Hearts, Jack)], (Clubs, Jack), NoSuchCard)); false
                           with NoSuchCard -> true | _ -> false)
     printfn "all_same_color:"
     printfn "Test1: %b" (all_same_color([]) = true)
+    printfn "Test2: %b" (all_same_color([(Clubs, Ace)]) = true)
+    printfn "Test3: %b" (all_same_color([(Clubs, Ace); (Hearts, Ace)]) = false)
+    printfn "Test4: %b" (all_same_color([(Clubs, Ace); (Clubs, Ace)]) = true)
     printfn "sum_cards:"
     printfn "Test1: %b" (sum_cards([(Clubs, Num 7)]) = 7)
+    printfn "Test2: %b" (sum_cards([]) = 0)
+    printfn "Test3: %b" (sum_cards([(Clubs, Ace); (Clubs, Ace)]) = 22)
+    printfn "Test4: %b" (sum_cards([(Clubs, King); (Clubs, Ace)]) = 21)
+    printfn "Test5: %b" (sum_cards([(Clubs, King); (Clubs, Queen)]) = 20)
+    printfn "Test6: %b" (sum_cards([(Clubs, King); (Clubs, Queen); (Clubs, Num 10)]) = 30)
+    printfn "Test7: %b" (sum_cards([(Clubs, Jack); (Clubs, Queen); (Clubs, Num 10)]) = 30)
     printfn "score:"
     printfn "Test1: %b" (score([(Clubs, Num 7)], 7) = 0)
     printfn "Test2: %b" (score([(Hearts, Num 2); (Clubs, Num 4)], 10) = 4)
+    printfn "Test3: %b" (score([(Hearts, Num 2); (Clubs, Num 4)], 2) = 12)
+    printfn "Test4: %b" (score([(Hearts, Num 2); (Hearts, Num 4)], 2) = 6)
+    printfn "Test5: %b" (score([(Hearts, Num 2); (Hearts, Ace)], 2) = 16)
     printfn "officiate:"
     printfn "Test1: %b" (officiate ([(Hearts, Num 2); (Clubs, Num 4)], [Draw], 15) = 6)
     printfn "Test2: %b" (officiate([(Clubs,Ace); (Spades,Ace); (Clubs,Ace); (Spades,Ace)],
@@ -61,6 +93,6 @@ let tests () =
     printfn "Test3: %b" (try 
                             ignore (officiate([(Clubs, Jack); (Spades, Num(8))], [Draw; Discard(Hearts, Jack)],42));
                             false
-                         with IllegalMove -> true | _ -> false) *)
+                         with IllegalMove -> true | _ -> false) 
                          
 tests()
